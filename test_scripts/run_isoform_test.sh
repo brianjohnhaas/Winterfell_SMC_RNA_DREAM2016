cba7083b2066f0ffc9503eda95d1c8af43526947
Brian Haas <brianjohnhaas@gmail.com>
2016-08-11 18:04:49 +0000

    add

---
+++ test_scripts/run_isoform_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+../../SMC-RNA-Challenge/script/dream_runner.py test $1 ~/Winterfell_SMC_RNA_DREAM2016/RSEM/cwl/workflow/smcIsoform-rsem-workflow.cwl isoform --dir ~ --cachedir ~/cwl-cache
